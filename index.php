9a6efe864c1745199f6856b3486e3969ddcd3591
MEDO114 <59060013+MEDO114@users.noreply.github.com>
2021-12-10 17:05:07 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 ///https://api.telegram.org/bot<token>/setwebhook?url=<url>
 
-$botToken = "1472905848:AAHj-wDq3UJKkCL61VnuK7w-zOPxadnKjlI"; // Enter ur bot token
+$botToken = "5090497725:AAFEEdfil9uwG7ZsIJ2QNdXvE8UYGA3PN-o"; // Enter ur bot token
 $website = "https://api.telegram.org/bot".$botToken;
 error_reporting(0);
 $update = file_get_contents('php://input');
